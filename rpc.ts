d4ac3cf4780e62e23e759e71ff01935c2a8cbf83
John Espenhahn <JohnEspenhahn@users.noreply.github.com>
2017-02-20 20:37:11 -0500

    Delete rpc.ts

--- rpc.ts
+++
@@ -1,9 +0,0 @@
-import { RMIRegistry } from "./public/rpc/RMIRegistry";
-import { RMIServerRegistry } from './private/rpc/RMIServerRegistry';
-
-export = function(server: any): RMIRegistry {
-  var io = require('socket.io')(server);
-
-  // Setup RMI
-  return RMIServerRegistry.get(io);
-};
